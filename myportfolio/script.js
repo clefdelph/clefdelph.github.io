56ad1b6892e2da7c9193c6feddf729d4255abb66
clefdelph <tk.3.dolpharm@gmail.com>
2018-02-24 18:37:46 +0900

    Update script.js

--- myportfolio/script.js
+++ myportfolio/script.js
@@ -11,6 +11,21 @@ $('a[href^=#]').click(function(){
         return false;
 
     });
+ 
+ $('.js-accordion').click(function() {
+    var $archive = $(this).find('.archive');
+    if ($archive.hasClass('open')) {
+    // true: open クラスがあれば
+      $archive.removeClass('open');
+      $archive.slideUp();
+      $(this).find('.toggle').text('+');
+    } else {
+    // false: open クラスがなければ
+      $archive.addClass('open');
+      $archive.slideDown();
+      $(this).find('.toggle').text('-');
+    }
+  });
 
 });
 
